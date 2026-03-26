7ad3dc2a303454122ebd309959bbf8b594c1315f
Norph1n <3422950742@qq.com>
2026-03-26 13:26:18 +0800

    Chapter4-3

--- Chapter4/func/func.c
+++ Chapter4/func/func.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
+
+int a,b,c;
+float delta;
 int main()
 {
-   print("请输入a，b，c，分别用空格隔开")
+   printf("请输入a，b，c，分别用空格隔开\n");
+   scanf("%d%d%d",&a,&b,&c);
+   delta=(b*b)-(4*a*c);
+   if(delta<0)
+      printf("该方程无实根\n");
+   else if(delta==0)
+      printf("该方程有一个重根\n");
+   else
+      printf("该方程有两个不同根\n");
    return 0;
 }
