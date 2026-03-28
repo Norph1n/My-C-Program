cf612fcdfa05568b406235ca5ae6afdbb3616d97
Norph1n <3422950742@qq.com>
2026-03-28 11:49:37 +0800

    Chapter5-1

---
+++ Chapter5/program1/program1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<windows.h>
+
+float h;
+
+int main()
+{
+    printf("请输入初始高度\n");
+    scanf("%f",&h);
+    for(int i=1;i<=10;i++)
+    {
+        h/=2;
+    }
+    printf("最后高度是：%f\n",h);
+    return 0;
+}
